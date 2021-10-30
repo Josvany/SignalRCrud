aea9de8a36ee0aa389f4a9936d6392d1f688dc6d
Josvany_G <Josvany_G@JOSVANY>
2021-10-30 16:31:08 -0600

    Agregar archivos de proyecto.

---
+++ SignalRCrud/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalRCrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SignalRCrud/wwwroot/js/site.js
@@ -0,0 +1,47 @@
+﻿
+
+$(() =>
+{
+    LoadProductData();
+
+    var connection = signalR.HubConnectionBuilder().withUrl("/singalrServer").build();
+
+    connection.start();
+    connection.on("LoadProducts", function ()
+    {
+        LoadProductData();
+    });
+
+    LoadProductData();
+
+    function LoadProductData()
+    {
+        var tr = '';
+
+        $.ajax({
+            url: '/products/GetProducts',
+            method: 'GET',
+            success: (result) =>
+            {
+                $.each(result, (k, v) =>
+                {
+                    tr += `<tr>
+                        <td>${v.ProdName}</td>
+                        <td>${v.Category}</td>
+                        <td>${v.UnitPrice}</td>
+                        <td>${v.Stock}</td>
+                        <td><a href='../Products/Edit?id=${v.ProdId}'>Edit</a></td>
+                        <td><a href='../Products/Details?id=${v.ProdId}'>Details</a></td>
+                        <td><a href='../Products/Delete?id=${v.ProdId}'>Delete</a></td>
+                    </tr>`
+                })
+
+                $("#tableBody").html(tr);
+            },
+            error: (error) =>
+            {
+                console.log(error);
+            }
+        });
+    }
+});
